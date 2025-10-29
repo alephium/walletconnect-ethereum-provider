1ab64274080cd4568dcbb7bfebf7394e6153f79e
lbqds <lbqds@outlook.com>
2025-10-30 07:50:23 +0800

    Copy Ethereum provider code from WalletConnect

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { EthereumProvider as Provider } from "./ethereum-provider/EthereumProvider";
-export const EthereumProvider = Provider;
-export type { EthereumProviderOptions, RpcEvent, RpcMethod } from "./ethereum-provider/EthereumProvider";
-export * from "./ethereum-provider/constants/rpc";
-export default Provider;
